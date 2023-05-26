fc4f6ce1fc38ea8cc0051395ce85d9ca32173511
Nicolas <nicolasgalvez1985@gmail.com>
2023-05-26 17:30:35 -0300

    Se agrega funcion para calculos de calorias a consumir

--- consola_calculo_calorias_adelgazar.py
+++ consola_calculo_calorias_adelgazar.py
@@ -2,4 +2,4 @@ from consola_calculo_calorias_reposo import *
 
 
 def consumo_calorias_recomendado_para_adelgazar(peso, altura, edad, valor_genero):
-    return f"Para adelgazar es recomendado que consumas entre: {calcular_calorias_en_reposo(peso, altura, edad, valor_genero)*0.8} y {calcular_calorias_en_reposo(peso, altura, edad, valor_genero)*0.85} calorías al día."
+    return f"Para adelgazar es recomendado que consumas entre: {calcular_calorias_en_reposo(peso, altura, edad, valor_genero)*0.8} y {round(calcular_calorias_en_reposo(peso, altura, edad, valor_genero),2)*0.85} calorías al día."
